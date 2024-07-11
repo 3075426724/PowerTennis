54aa4a95cce44382f96b05f2bbb1bb147b097ba1
3075426724@126.com <3075426724@126.com>
2024-07-11 19:29:22 +0800

    CR

---
+++ cloudfunctions/cloud/project/controller/my_controller.js
@@ -0,0 +1,15 @@
+/**
+ * Notes: 用户中心模块控制器
+ * Date: 2021-03-15 19:20:00 
+ * Ver : CCMiniCloud Framework 2.0.1 ALL RIGHTS RESERVED BY cclinux0730 (wechat)
+ */
+
+const BaseController = require('./base_controller.js'); 
+class MyController extends BaseController {
+ 
+
+ 
+
+}
+
+module.exports = MyController;
